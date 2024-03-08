aeab089cfbf4e243ac69c208a988538b4cb7cdb6
fik123-sudo <75900674+fik123-sudo@users.noreply.github.com>
2024-03-08 04:00:06 -0800

    revision historey

--- server/src/server.js
+++ server/src/server.js
@@ -3,5 +3,5 @@ const app = require('./app')
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 server.listen(PORT,()=>{
-    console.log(`server is listning on port ${PORT}`);
+    console.log(`server is on port ${PORT}`);
 });
