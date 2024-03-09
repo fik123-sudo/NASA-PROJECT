fa23959ed02f476ace9b9ae38eb5b78a173cefb2
fik123-sudo <75900674+fik123-sudo@users.noreply.github.com>
2024-03-09 04:14:14 -0800

    revision

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,9 @@
 const http = reqire('http');
+
 const app = require('./app')
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 server.listen(PORT,()=>{
     console.log(`server is on port ${PORT}`);
+Console.log('hello');
 });
